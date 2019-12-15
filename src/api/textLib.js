9a193881165749007946dac231e49e3ecfbc02eb
albert8202 <853292199@qq.com>
2019-12-15 12:22:06 +0800

    a

--- src/api/textLib.js
+++ src/api/textLib.js
@@ -8,4 +8,5 @@ export default {
       method: 'get',
     })
   }
+
 }
--- src/components/MyProject.vue
+++ src/components/MyProject.vue
@@ -4,31 +4,56 @@
     <span>
 
     </span>
-    <div>
+    <div style="width: 70%;margin-left: 15%">
+      <el-button type="text" size="small" style="margin-left: 100px" @click="isLibCreateShow = true"><i class="el-icon-upload el-icon--right"></i>新建文本库</el-button>
+      <!--create new textLib-->
+      <el-dialog title="新建文本库" :visible.sync="isLibCreateShow" style="width: 60%;margin-left: 20%">
+        <el-form :model="form">
+          <el-form-item label="文本库名称" style="width: 80%;margin-left: 10%">
+            <el-input v-model="newLibInfor.name"
+                      placeholder="请输入文本库的标题"
+                      autocomplete="off">
+            </el-input>
+          </el-form-item>
+          <el-form-item label="文本库描述" style="width: 80%;margin:10% auto">
+            <el-input v-model="newLibInfor.desc"
+                      type="textarea"
+                      placeholder="请输入文本库的描述"
+                      autocomplete="off"
+                      :autosize="{ minRows: 3, maxRows: 5}">
+            </el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="isLibCreateShow = false">取 消</el-button>
+          <el-button type="primary" @click="isLibCreateShow = false">新 建</el-button>
+        </div>
+      </el-dialog>
+
       <el-table
         ref="mutipleTable"
         :data="tableData"
         tooltip-effect="dark"
-        style="width: 1600px;margin-left: 100px"
         @selection-change="handleSelectionChange"
       >
-        <el-table-column prop="libId"  label="序号" width="55" align="center"></el-table-column>
-        <el-table-column prop="libTitle" label="文本库标题" width="700" align="center"></el-table-column>
-        <el-table-column prop="libDesc" label="描述" width="100" style="color: dimgray" align="center"></el-table-column>
-        <el-table-column prop="libNum" label="数量" width="300" align="center"></el-table-column>
-        <el-table-column prop="createTime" label="创建时间" width="200" align="center"></el-table-column>
-        <el-table-column  width="200" align="center">
-<!--          <el-dropdown  type="primary">-->
-<!--            <span class="el-dropdown-link">-->
-<!--              操作-->
-<!--              <i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>-->
-<!--            </span>-->
-<!--            <el-dropdown-menu slot="dropdown">-->
-<!--              <el-dropdown-item command="data">数据</el-dropdown-item>-->
-<!--              <el-dropdown-item command="delete">删除</el-dropdown-item>-->
-<!--            </el-dropdown-menu>-->
-<!--          </el-dropdown>-->
-          <el-dropdown @command="handleCommand">
+
+        <el-table-column prop="libId"  label="序号" style="width: 5%" align="center"></el-table-column>
+        <el-table-column prop="libTitle" label="文本库标题" style="width: 20%" align="center"></el-table-column>
+        <el-table-column prop="libDesc" label="描述"  style="color: dimgray;width: 30%" align="center"></el-table-column>
+        <el-table-column prop="libNum" label="数量" style="width: 10%" align="center"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间" style="width: 15%" align="center"></el-table-column>
+        <el-dropdown>
+  <span class="el-dropdown-link">
+    下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+  </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
   <span class="el-dropdown-link">
     下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
   </span>
@@ -143,7 +168,12 @@ export default {
           libNum: "233",
           createTime: "2018-04-21"
         }
-      ]
+      ],
+      isLibCreateShow:false,
+      newLibInfor:{
+        name:'',
+        desc:''
+      }
     };
   },
     methods:{
